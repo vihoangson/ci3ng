cf4e35efc0ee545ffc82ccfd948e2a3fadb5c77a
Dzung Nguyen <ntd1712@gmail.com>
2016-12-17 12:13:25 +0700

    Update Controller class

--- src/application/shared/Classes/Controller.php
+++ src/application/shared/Classes/Controller.php
@@ -1,6 +1,7 @@
 <?php namespace Shared\Classes;
 
-use Chaos\Common\AbstractCodeIgniterRestController;
+use Doctrine\DBAL\Logging\DebugStack,
+    Chaos\Common\AbstractCodeIgniterRestController;
 
 /**
  * Class Controller
@@ -11,18 +12,42 @@ abstract class Controller extends AbstractCodeIgniterRestController
     /** {@inheritdoc} */
     public function __construct()
     {
+        global $active_group, $db;
+        require_once APPPATH . 'config/database.php';
+
         parent::__construct(
             array_replace_recursive([
                 'app' => $config = get_config([
                     'key' => env('APP_KEY')
                 ]),
-                'session' => [
-                    'cookie' => $config['sess_cookie_name'],
-                    'expires' => $config['sess_expiration'],
-                    'path' => $config['cookie_path'],
-                    'domain' => $config['cookie_domain'],
-                    'secure' => $config['cookie_secure'],
-                    'http_only' => $config['cookie_httponly']
+                'db' => [
+                    'driver' => $db[$active_group]['dbdriver'],
+                    'user' => $db[$active_group]['username'],
+                    'host' => $db[$active_group]['hostname'],
+                    'dbname' => $db[$active_group]['database'],
+                    'charset' => $db[$active_group]['char_set']
+                ] + $db[$active_group],
+                'orm' => [
+                    'cache' => [
+                        'provider' => env('CACHE_DRIVER', 'array'),
+                        'filesystem' => [
+                            'directory' => APPPATH . 'cache',
+                            'extension' => '.' . $config['cookie_prefix'] . '.data'
+                        ]
+                    ],
+                    'metadata' => [
+                        'driver' => 'annotation',
+                        'paths' => require_once __DIR__ . '/../Modules/doctrine.paths.php',
+                        'simple' => false
+                    ],
+                    'proxy' => [
+                        'auto_generate' => $db[$active_group]['db_debug'] ? 2 : 0,
+                        'dir' => APPPATH . 'cache/proxies',
+                        'namespace' => null,
+                    ],
+                    'default_repository' => 'Doctrine\ORM\EntityRepository',
+                    'is_dev_mode' => $db[$active_group]['db_debug'],
+                    'sql_logger' => $db[$active_group]['db_debug'] ? new DebugStack : null,
                 ],
                 'mail' => [
                     'driver' => env('MAIL_DRIVER'),
@@ -31,6 +56,14 @@ abstract class Controller extends AbstractCodeIgniterRestController
                     'encryption' => env('MAIL_ENCRYPTION'),
                     'username' => env('MAIL_USERNAME'),
                     'password' => env('MAIL_PASSWORD')
+                ],
+                'session' => [
+                    'cookie' => $config['sess_cookie_name'],
+                    'expires' => $config['sess_expiration'],
+                    'path' => $config['cookie_path'],
+                    'domain' => $config['cookie_domain'],
+                    'secure' => $config['cookie_secure'],
+                    'http_only' => $config['cookie_httponly']
                 ],
                 'paths' => ['config' => $configPath = __DIR__ . '/../Modules/config.params.php'],
             ], require_once $configPath),
